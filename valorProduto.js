4513862c2d3c00772a322862a6eddd22b46b1d17
Laura Olegário <109252750+Lauolegario@users.noreply.github.com>
2022-10-24 12:14:30 -0300

    Atualizado com a utilização de funções
    Melhoria da aplicação utilizando funções.

--- valorProduto.js
+++ valorProduto.js
@@ -1,13 +1,19 @@
-const precoProduto = 92;
+const precoProduto = 100;
 let formaPagamento = 4;
 
+function aplicarDesconto(valor, desconto) {
+   return valor - (valor * (desconto / 100));
+}
+function aplicarJuros(valor, juros) {
+   return valor + (valor * (juros / 100));
+}
 
-if (formaPagamento === 1){
-   console.log(precoProduto - ( precoProduto* 0.10));
-}else if (formaPagamento === 2){
-   console.log(precoProduto -( precoProduto * 0.15));
-}else if (formaPagamento === 3){
-   console.log( precoProduto /2)
-}else if (formaPagamento === 4){
-   console.log(precoProduto + (precoProduto * 0.1))
+if (formaPagamento === 1) {
+   console.log(aplicarDesconto(precoProduto, 10));
+} else if (formaPagamento === 2) {
+   console.log(aplicarDesconto(precoProduto, 15));
+} else if (formaPagamento === 3) {
+   console.log(precoProduto / 2);
+} else if (formaPagamento === 4) {
+   console.log(aplicarJuros(precoProduto, 10));
 }
